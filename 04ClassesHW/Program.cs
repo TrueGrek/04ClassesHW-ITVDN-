ef6a5ae49bd617a4d96ec37d9b17a356d8de5970
Grek <grrekov@yandex.ru>
2020-03-16 15:11:28 +0300

    Добавьте файлы проекта.

---
+++ 04ClassesHW/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _04ClassesHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TXTHandler ex = new TXTHandler();
+            ex.Change();
+            XMLHandler el = new XMLHandler();
+            el.Change();
+
+        }
+    }
+
+    abstract class AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Craete();
+        public abstract void Change();
+        public abstract void Save();
+    }
+        class XMLHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("XML документ открывается.");
+        }
+        public override void Craete()
+        {
+            Console.WriteLine("XML документ создаётся.");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("XML документ изменяется.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("XML документ сохраняется.");
+        }
+    }
+        class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("TXT документ открывается.");
+        }
+        public override void Craete()
+        {
+            Console.WriteLine("TXT документ создаётся.");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("TXT документ изменяется.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("TXT документ сохраняется.");
+        }
+    }
+        class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("DOC документ открывается.");
+        }
+        public override void Craete()
+        {
+            Console.WriteLine("DOC документ создаётся.");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("DOC документ изменяется.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("DOC документ сохраняется.");
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player pla = new Player();
+            (pla as IPlayable).Stop();
+            (pla as IRecordable).Stop();
+        }
+    }
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecordable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : IPlayable, IRecordable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play music");
+        }
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Pause music");
+        }
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stop music");
+        }
+        public void Record()//неявно так как он у нас в конкретном интерфейсе, а не в двух.
+        {
+            Console.WriteLine("Record music");
+        }
+        void IRecordable.Pause()//явно так как одноименные методы.
+        {
+            Console.WriteLine("Pause record");
+        }
+        void IRecordable.Stop()
+        {
+            Console.WriteLine("Stop record");
+        }
+    }
+}
